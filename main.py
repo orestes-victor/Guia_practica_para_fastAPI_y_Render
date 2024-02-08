443d941c2f1a27db9db9412dece20ea6f9cc89a1
Victor <orestesvictor@gmail.com>
2024-02-07 22:14:12 -0300

    Actualizo todo

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ app = FastAPI()
 
 # Función para obtener el top de gastadores por año
 def top_consumidores_por_anio(df, year):
-
+    """
+    Obtiene el top 3 de usuarios que más gastaron en un año específico.
+    :param df: DataFrame con datos de gastos
+    :param year: Año para el cual se quiere obtener el top de consumidores
+    :return: Diccionario con la información del top de consumidores
+    """
     # Filtrar el DataFrame por el año proporcionado
     df_usuarios_filtered = df[df['Año'] == year]
 
@@ -22,12 +27,15 @@ def top_consumidores_por_anio(df, year):
     return result
 
 # Ruta para obtener el top de consumidores por año desde el archivo Parquet
-@app.get("/top3_consumidores/{year}")
+@app.get("/top3_consumidores/{year}", response_model=dict)
 async def top_consumidores_by_year(year: int):
+    """
+    Obtiene el top de consumidores para un año específico desde un archivo Parquet.
+    Por ejemplo: ingresamos 1990
+    """
     try:
         # Cambia la ruta del archivo según la ubicación de tu archivo df_endpoint_1.parquet
         df = pd.read_parquet("C:/Users/Usuario/Desktop/Repositorios Github/Repositorio_guia_para_fastAPI_y_RENDER/Dataset/dataset_endpoint_1.parquet")
-        # df = pd.read_parquet("df_endpoint_1.parquet")  # Esta línea está duplicada y se puede eliminar
 
         # Aplicar la función para obtener el top de gastadores por año
         result = top_consumidores_por_anio(df, year)
